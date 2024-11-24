008389351581c1d5a4170370bfc5e7c88cda00e7
Quinowl <quinowldev@gmail.com>
2024-11-24 20:03:47 +0100

    Fixed pressure plate

--- Elder Path/Assets/ElderPath/Scripts/Components/PressurePlate.cs
+++ Elder Path/Assets/ElderPath/Scripts/Components/PressurePlate.cs
@@ -1,22 +1,35 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PressurePlate : MonoBehaviour {
 
-    public bool IsActive { get; private set; }
-
     public Action OnChangeState;
+    public bool IsActive { get; private set; }
+    private readonly List<string> validTags = new List<string> { "Player", "Rock" };
+    private List<Collider2D> validObjectsOnPlate = new List<Collider2D>();
 
     private void OnTriggerEnter2D(Collider2D other) {
-        ToggleActive(true);
+        if (IsValidObject(other) && !validObjectsOnPlate.Contains(other)) {
+            validObjectsOnPlate.Add(other);
+            UpdateState();
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        ToggleActive(false);
+        if (validObjectsOnPlate.Contains(other)) {
+            validObjectsOnPlate.Remove(other);
+            UpdateState();
+        }
     }
 
-    private void ToggleActive(bool active) {
-        IsActive = active;
-        OnChangeState?.Invoke();
+    private void UpdateState() {
+        bool newState = validObjectsOnPlate.Count > 0;
+        if (newState != IsActive) {
+            IsActive = newState;
+            OnChangeState?.Invoke();
+        }
     }
+
+    private bool IsValidObject(Collider2D collider2D) => validTags.Contains(collider2D.tag);
 }
