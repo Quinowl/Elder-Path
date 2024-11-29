bf735d67bd6b53bba75a06ac3a544820eaa809e0
Quinowl <quinowldev@gmail.com>
2024-11-29 11:39:23 +0100

    Started sound manager

---
+++ Elder Path/Assets/ElderPath/Scripts/Components/AudioPlayer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlayer : MonoBehaviour {
+
+    [SerializeField] private AudioSource audioSource;
+
+    private float lifeTime;
+
+    private void Awake() {
+        if (!audioSource) audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update() {
+        if (Time.time > lifeTime) EPSoundManager.Instance.AudioPlayerPool.ReturnToPool(this);
+    }
+
+    public void Initialize(AudioClip audioClip, float volume) {
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        audioSource.Play();
+        lifeTime = audioSource.clip.length + Time.time;
+    }
+}
