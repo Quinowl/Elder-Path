2e723e3a11c49bf586cc42cb6819c380bf0b7c0f
Quinowl <quinowldev@gmail.com>
2024-12-07 19:33:40 +0100

    Nomenclature issue

--- Elder Path/Assets/ElderPath/Scripts/Levels/Level.cs
+++ Elder Path/Assets/ElderPath/Scripts/Levels/Level.cs
@@ -2,14 +2,14 @@ using System;
 using UnityEngine;
 
 public class Level : MonoBehaviour {
+    [SerializeField] private FinalLevelTrigger finalTrigger;
     [field: SerializeField] public int LevelIndex { get; private set; }
     [field: SerializeField] public Transform InitialPlayerPosition { get; private set; }
-    [field: SerializeField] public FinalLevelTrigger FinalTrigger { get; private set; }
     public static Action<Level> OnLevelLoaded;
 
     public void InitializeLevel() {
         OnLevelLoaded?.Invoke(this);
-        FinalTrigger.Configure(this);
+        finalTrigger.Configure(this);
     }
 
     public void EndLevel() => ServiceLocator.Instance.GetService<EPLevelManager>().LoadNextLevel();
