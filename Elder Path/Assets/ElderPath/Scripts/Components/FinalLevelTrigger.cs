21788534e9fc2a88e5e092338b29d40df68d045c
Quinowl <quinowldev@gmail.com>
2024-12-09 22:12:59 +0100

    Fixed final trigger issue

--- Elder Path/Assets/ElderPath/Scripts/Components/FinalLevelTrigger.cs
+++ Elder Path/Assets/ElderPath/Scripts/Components/FinalLevelTrigger.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class FinalLevelTrigger : MonoBehaviour {
 
+    [SerializeField] private Collider2D collider2d;
     private Level currentLevel;
 
-    public void Configure(Level level) => currentLevel = level;
+    private void Awake() {
+        if (!collider2d) collider2d = GetComponent<Collider2D>();
+    }
+
+    public void Configure(Level level) {
+        currentLevel = level;
+        collider2d.enabled = true;
+    }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) currentLevel.EndLevel();
