811f9321cad1aadced0c46c327ee676a4ff646ce
Quinowl <quinowldev@gmail.com>
2024-12-13 02:27:12 +0100

    Spears trap can detect the player

---
+++ Elder Path/Assets/ElderPath/Scripts/Traps/PlayerDetecter.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class PlayerDetecter : MonoBehaviour {
+
+    public Action OnPlayerDetected;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag(Constants.Tags.PLAYER)) OnPlayerDetected?.Invoke();
+    }
+}
--- Elder Path/Assets/ElderPath/Scripts/Traps/SpearsTrap.cs
+++ Elder Path/Assets/ElderPath/Scripts/Traps/SpearsTrap.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class SpearsTrap : MonoBehaviour {
 
+    [Header("References")]
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private PlayerDetecter playerDetecter;
 
     [SerializeField] private Sprite idleSprite;
     [SerializeField] private Sprite fallingSprite;
@@ -11,5 +13,17 @@ public class SpearsTrap : MonoBehaviour {
         if (!idleSprite || !fallingSprite) Debug.LogError("Idle sprite or falling sprite is not configured.");
         if (!spriteRenderer) spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         spriteRenderer.sprite = idleSprite;
+    }
+
+    private void Start() {
+        playerDetecter.OnPlayerDetected += FallSpears;
+    }
+
+    private void OnDestroy() {
+        playerDetecter.OnPlayerDetected -= FallSpears;
+    }
+
+    private void FallSpears() {
+        Debug.Log("Player detected");
     }
 }
