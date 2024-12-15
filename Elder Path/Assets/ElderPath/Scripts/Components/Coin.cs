01088b6f645e8c3a75bc532189e1b259025df305
Quinowl <quinowldev@gmail.com>
2024-12-16 00:09:24 +0100

    Added coin sound and fixed coin collision issue

--- Elder Path/Assets/ElderPath/Scripts/Components/Coin.cs
+++ Elder Path/Assets/ElderPath/Scripts/Components/Coin.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 public class Coin : MonoBehaviour {
 
     [SerializeField] private Animator animator;
+    [SerializeField] private Collider2D collider2d;
 
     private void Awake() {
         if (!animator) animator = GetComponent<Animator>();
+        if (!collider2d) collider2d = GetComponent<Collider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.TryGetComponent(out PlayerController player)) {
             //TODO: Add coin to player
             animator.Play(Constants.MiscAnimations.COIN_PICK_UP);
+            ServiceLocator.Instance.GetService<EPSoundsManager>().PlaySFX(Constants.SFXIDs.MISC_PICK_UP_COIN, transform);
+            collider2d.enabled = false;
             Destroy(gameObject, .75f);
         }
     }
--- Elder Path/Assets/ElderPath/Scripts/Constants/Constants.SFXIDs.cs
+++ Elder Path/Assets/ElderPath/Scripts/Constants/Constants.SFXIDs.cs
@@ -2,5 +2,6 @@ public static partial class Constants {
     public static class SFXIDs {
         public const string PLAYER_JUMP = "sfx-player-jump";
         public const string PLAYER_DASH = "sfx-player-dash";
+        public const string MISC_PICK_UP_COIN = "coin-pick-up";
     }
 }
