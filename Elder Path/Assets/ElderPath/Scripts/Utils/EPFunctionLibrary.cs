b4c4db7eb3f75b9dd6115f29c8776b38df68b617
Quinowl <quinowldev@gmail.com>
2024-12-15 19:46:43 +0100

    Dust particles to jump and movement adjusted

--- Elder Path/Assets/ElderPath/Scripts/Utils/EPFunctionLibrary.cs
+++ Elder Path/Assets/ElderPath/Scripts/Utils/EPFunctionLibrary.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public static class EPFunctionLibrary {
 
+    // ===== GAMEPLAY =====
+    public static void ClearChilds(this Transform transform) {
+        for (int i = 0; i < transform.childCount; i++) {
+            GameObject.Destroy(transform.GetChild(i).gameObject);
+        }
+    }
+
     // ===== UI ======
     public static void Toggle(this CanvasGroup canvasGroup, bool enable) {
         canvasGroup.alpha = enable ? 1f : 0f;
