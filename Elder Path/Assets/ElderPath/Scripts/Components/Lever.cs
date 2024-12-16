3af39990b9c194d4b2e635c95b1b066670aa668b
Quinowl <quinowldev@gmail.com>
2024-12-16 15:15:30 +0100

    Added level behaviour

--- Elder Path/Assets/ElderPath/Scripts/Components/Lever.cs
+++ Elder Path/Assets/ElderPath/Scripts/Components/Lever.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
 
 public class Lever : MonoBehaviour, IHittable {
+
+    [SerializeField] private Sprite activatedSprite;
+    [SerializeField] private Sprite unactivatedSprite;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private bool isActivated;
+
+    private void Awake() {
+        if (!activatedSprite || !unactivatedSprite) Debug.LogError("Level has not sprites assigned.");
+        if (!spriteRenderer) spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        isActivated = false;
+        spriteRenderer.sprite = unactivatedSprite;
+        //TODO: Modificar el sprite de activado para que se vea cuándo lo está y cuando no
+    }
+
     public void Hit(HitContext context) {
+        isActivated = !isActivated;
+        spriteRenderer.sprite = isActivated ? activatedSprite : unactivatedSprite;
     }
 }
