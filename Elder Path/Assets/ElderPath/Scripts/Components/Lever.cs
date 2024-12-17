c23f14cd5c0309b5a7fe8b0b8b271d4220cf8b71
Quinowl <quinowldev@gmail.com>
2024-12-17 13:50:59 +0100

    Added attack level tutorial

--- Elder Path/Assets/ElderPath/Scripts/Components/Lever.cs
+++ Elder Path/Assets/ElderPath/Scripts/Components/Lever.cs
@@ -5,17 +5,17 @@ public class Lever : MonoBehaviour, IHittable {
     [SerializeField] private Sprite activatedSprite;
     [SerializeField] private Sprite unactivatedSprite;
     [SerializeField] private SpriteRenderer spriteRenderer;
-    [SerializeField] private LeverWall[] wallsAssociated;
+    [SerializeField] private LeverWall[] lasersAssociated;
 
     private bool isActivated;
 
     private void OnDrawGizmos() {
-        if (wallsAssociated == null || wallsAssociated.Length <= 0) {
+        if (lasersAssociated == null || lasersAssociated.Length <= 0) {
             Debug.LogError("Lever has no walls associated.");
             return;
         }
         Gizmos.color = Color.grey;
-        foreach (var wall in wallsAssociated) {
+        foreach (var wall in lasersAssociated) {
             Gizmos.DrawLine(transform.position, wall.transform.position);
         }
     }
@@ -25,12 +25,11 @@ public class Lever : MonoBehaviour, IHittable {
         if (!spriteRenderer) spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         isActivated = false;
         spriteRenderer.sprite = unactivatedSprite;
-        //TODO: Modificar el sprite de activado para que se vea cuándo lo está y cuando no
     }
 
     public void Hit(HitContext context) {
         isActivated = !isActivated;
         spriteRenderer.sprite = isActivated ? activatedSprite : unactivatedSprite;
-        foreach (var wall in wallsAssociated) wall.ChangeState();
+        foreach (var wall in lasersAssociated) wall.ChangeState();
     }
 }
