2e20383a53673a7a25c19dcf1d6ea26b73b9d2f3
Quinowl <quinowldev@gmail.com>
2024-12-17 01:10:13 +0100

    Finished lever and laser behaviours.

--- Elder Path/Assets/ElderPath/Scripts/Components/LeverWall.cs
+++ Elder Path/Assets/ElderPath/Scripts/Components/LeverWall.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class LeverWall : MonoBehaviour {
 
+    [Header("References")]
     [SerializeField] private SpriteRenderer laserRenderer;
     [SerializeField] private Collider2D laserCollider;
+    [Header("Configuration")]
+    [SerializeField, Range(0f, 1f)] private float inactiveAlpha = 0.3f;
+    [SerializeField] private bool isActiveAtStart = true;
 
     private bool isActive;
 
     private void Awake() {
         if (!laserRenderer) Debug.LogError("No laser renderer assigned.");
+        if (!laserCollider) Debug.LogError("No laser collider assigned.");
         SetRendererAlpha(1f);
-        isActive = true;
+        isActive = isActiveAtStart;
+        laserCollider.enabled = isActive;
+        SetRendererAlpha(isActive ? 1f : inactiveAlpha);
     }
 
     public void ChangeState() {
